30f897efb0f29be204a64b90a7deb11f731db91f
PaleVerge <fnme370679@gmail.com>
2026-08-01 22:02:50 +0800

    加了一些注释

--- 2-基础算法/枚举/幸运数.cpp
+++ 2-基础算法/枚举/幸运数.cpp
@@ -3,7 +3,7 @@
 //
 #include <iostream>
 using namespace std;
-int digitcnt(int i){
+int digitcnt(int i){//计算i的位数
     int count=0;
     while (i){
         count++;
@@ -21,13 +21,13 @@ int digitcnt1(int n){
 bool islucky(int n){
     int dcnt=digitcnt(n);
 
-    if (dcnt&1)return false;
+    if (dcnt&1)return false//奇数->不是
 
     int front=0,back=0,count=0;
     while (n){
         count++;
         int digit=n%10;
-        if (count<=(dcnt>>1)) front+=digit;
+        if (count<=(dcnt>>1)) front+=digit;//当前位数 < 总位数的1/2
         else back+=digit;
         n/=10;
     }
