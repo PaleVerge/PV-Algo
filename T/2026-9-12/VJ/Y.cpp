661ea8d3427a445dba25e0bca5748d502f87ab00
PaleVerge <fnme370679@gmail.com>
2026-09-15 23:46:14 +0800

    9-15

--- T/2026-9-12/VJ/Y.cpp
+++ T/2026-9-12/VJ/Y.cpp
@@ -1,3 +1,36 @@
 //
 // Created by whip on 2026/9/13.
 //
+#include <bits/stdc++.h>
+using namespace std;
+map<char,int> m1,m2;
+vector<int> v1,v2;
+int main(){
+    string s1,s2;
+    cin>>s1>>s2;
+    for (int i=0;i<s1.size();++i){
+        m1[s1[i]]++;
+        m2[s2[i]]++;
+    }
+    for (auto & x:m1){
+        v1.push_back(x.second);
+    }
+    for (auto & x:m2){
+        v2.push_back(x.second);
+    }
+    if (v1.size()!=v2.size()){
+        cout<<"NO"<<endl;
+        return 0;
+    }
+
+    sort(v1.begin(),v1.end());
+    sort(v2.begin(),v2.end());
+
+    for (int i=0;i<v1.size();++i){
+        if (v1[i]!=v2[i]){
+            cout<<"NO"<<endl;
+            return 0;
+        }
+    }
+    cout<<"YES";
+}
